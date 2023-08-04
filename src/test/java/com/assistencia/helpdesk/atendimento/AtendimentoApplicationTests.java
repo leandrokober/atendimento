cab3e2a2979689b93dcb21cc8378c597fd011034
Leandro Kober <lkober@hotmail.com>
2023-08-04 08:05:26 -0300

    Criação do Projeto

---
+++ src/test/java/com/assistencia/helpdesk/atendimento/AtendimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assistencia.helpdesk.atendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtendimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
